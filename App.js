9c20536d3fa33b516b49e8a35e37c63a14ace4d6
marijan <marijan@todah.hr>
2022-05-20 14:54:03 +0200

    Change the welcome message

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Codemagic = a little bit of code and a whole lotta magic!</Text>
       <StatusBar style="auto" />
     </View>
   );
